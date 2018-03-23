2faa89bbe6af66914fed9c7bd1cebe008dea0908
Lucas Moura <lucas.moura128@gmail.com>
2018-03-23 10:22:01 -0300

    Update run_submission script

--- house_prices/scripts/run_submission.sh
+++ house_prices/scripts/run_submission.sh
@@ -9,7 +9,11 @@ DATA_FOLDER='data/'
 
 TRAIN_FILE=$DATA_FOLDER$COMPETITION_NAME'/train.csv'
 TEST_FILE=$DATA_FOLDER$COMPETITION_NAME'/test.csv'
+NUM_EPOCHS=60
+BATCH_SIZE=32
 
 python submission.py \
   --train-file=${TRAIN_FILE} \
-  --test-file=${TEST_FILE}
+  --test-file=${TEST_FILE} \
+  --num-epochs=${NUM_EPOCHS} \
+  --batch-size=${BATCH_SIZE}
