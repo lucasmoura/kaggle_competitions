57c1db5b94527f5af0586f9645f775a9b00d8362
Lucas Moura <lucas.moura128@gmail.com>
2018-04-03 13:50:21 -0300

    Refactor dataset.py

--- house_prices/preprocessing/dataset.py
+++ house_prices/preprocessing/dataset.py
@@ -1,110 +1,144 @@
 import pandas as pd
 import numpy as np
 
+from scipy.special import boxcox1p
+from scipy.stats import skew
+
 
 class Dataset:
 
-    def __init__(self, dataset_path, train=True, verbose=True):
-        self.dataset_path = dataset_path
-        self.train = train
+    def __init__(self, train_path, test_path, verbose=True):
+        self.train_path = train_path
+        self.test_path = test_path
         self.verbose = verbose
 
-    def load_dataset(self):
-        self.dataset = pd.read_csv(self.dataset_path)
+        self.categorical_cols = {}
+        self.numeric_cols = {}
+        self.bucket_cols = {}
+
+    def load_datasets(self):
+        self.train = pd.read_csv(self.train_path)
+        self.test = pd.read_csv(self.test_path)
 
         if self.verbose:
             self.print_shape()
 
     def print_shape(self):
-        print('Dataset shape ', self.dataset.shape)
+        print('Train shape ', self.train.shape)
+        print('Test shape ', self.test.shape)
 
-    def encode_street(self):
-        self.dataset['enc_street'] = pd.get_dummies(
-            self.dataset.Street, drop_first=True)
+    def remove_above_ground_area_outliers(self):
+        self.train = self.train.drop(
+            self.train[
+                (self.train['GrLivArea'] > 4000) &
+                (self.train['SalePrice'] < 300000)].index
+        )
 
-    def encode_sales_condition(self):
-        self.dataset['enc_condition'] = self.dataset.SaleCondition.apply(
-            lambda x: 1 if x == 'Partial' else 0)
+    def fill_values_with_none(self, dataset):
+        fill_none = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu',
+                     'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond',
+                     'MasVnrType', 'MSSubClass', 'BsmtQual', 'BsmtCond',
+                     'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2']
 
-    def apply_feature_scaling(self, column_name):
-        column_values = self.dataset[column_name]
-        column_mean = self.dataset[column_name].mean()
-        column_std = self.dataset[column_name].std()
+        for column in fill_none:
+            dataset[column] = dataset[column].fillna('None')
 
-        self.dataset[column_name] = (column_values - column_mean) / column_std
+        return dataset
 
-    def update_ms_sub_class(self):
-        values = [20, 30, 40, 45, 50, 60, 70, 75,
-                  80, 85, 90, 120, 150, 160, 180, 190]
-        value_dict = dict(zip(values, range(len(values))))
-        self.dataset['MSSubClass'] = self.dataset.MSSubClass.apply(
-            lambda x: value_dict[x])
+    def fill_values_with_zero(self, dataset):
+        fill_zero = ['BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF',
+                     'BsmtFullBath', 'BsmtHalfBath', 'MasVnrArea',
+                     'GarageYrBlt', 'GarageArea', 'GarageCars']
+
+        for column in fill_zero:
+            dataset[column] = dataset[column].fillna(0)
+
+        return dataset
+
+    def fill_values_with_mode(self, dataset):
+        dataset['Electrical'] = dataset['Electrical'].fillna(
+            dataset['Electrical'].mode()[0])
+        dataset['KitchenQual'] = dataset['KitchenQual'].fillna(
+            dataset['KitchenQual'].mode()[0])
+        dataset['Exterior1st'] = dataset['Exterior1st'].fillna(
+            dataset['Exterior1st'].mode()[0])
+        dataset['Exterior2nd'] = dataset['Exterior2nd'].fillna(
+            dataset['Exterior2nd'].mode()[0])
+        dataset['SaleType'] = dataset['SaleType'].fillna(
+            dataset['SaleType'].mode()[0])
+        dataset['MSZoning'] = dataset['MSZoning'].fillna(
+            dataset['MSZoning'].mode()[0])
+        dataset["Functional"] = dataset["Functional"].fillna("Typ")
+
+        return dataset
+
+    def fill_values_with_median(self, dataset):
+        dataset['LotFrontage'] = dataset.groupby(
+            'Neighborhood')['LotFrontage'].transform(
+                lambda x: x.fillna(x.median()))
+
+        return dataset
+
+    def column_values_to_str(self, dataset):
+        dataset['MSSubClass'] = dataset['MSSubClass'].apply(str)
+        dataset['OverallQual'] = dataset['OverallQual'].apply(str)
+        dataset['OverallCond'] = dataset['OverallCond'].astype(str)
+        dataset['YrSold'] = dataset['YrSold'].astype(str)
+        dataset['MoSold'] = dataset['MoSold'].astype(str)
+
+        return dataset
+
+    def drop_values(self, dataset):
+        dataset = dataset.drop(['Utilities'], axis=1)
+        dataset = dataset.drop(['SalePrice'], axis=1)
+
+        return dataset
+
+    def create_new_variable(self, dataset):
+        dataset['TotalSF'] = dataset['TotalBsmtSF'] + dataset['1stFlrSF'] + dataset['2ndFlrSF']  # noqa
+        return dataset
+
+    def create_categorical_columns(self, dataset):
+        self.categorical_cols = {}
+
+        for value in dataset.select_dtypes(include=[np.object]):
+            self.categorical_cols[value] = dataset[value].unique().tolist()
+
+    def create_numeric_columns(self, dataset):
+        self.numeric_cols = dataset.columns.tolist() - self.categorical_cols.keys()  # noqa
 
     def create_dataset(self):
-        self.load_dataset()
-        self.encode_street()
-        self.encode_sales_condition()
-        self.update_ms_sub_class()
+        self.load_datasets()
+        self.remove_above_ground_area_outliers()
+        self.test_id = self.test['Id']
 
-        #self.apply_feature_scaling('LotFrontage')
-        #self.apply_feature_scaling('LotArea')
-        #self.apply_feature_scaling('MasVnrArea')
-        #self.apply_feature_scaling('BsmtFinSF1')
-        #self.apply_feature_scaling('BsmtFinSF2')
-        #self.apply_feature_scaling('1stFlrSF')
-        #self.apply_feature_scaling('2ndFlrSF')
-        #self.apply_feature_scaling('GarageArea')
-        #self.apply_feature_scaling('WoodDeckSF')
-        #self.apply_feature_scaling('OpenPorchSF')
-        #self.apply_feature_scaling('EnclosedPorch')
-        #self.apply_feature_scaling('ScreenPorch')
+        ntrain = self.train.shape[0]
+        y_train = self.train.SalePrice.values
 
+        dataset = pd.concat((self.train, self.test)).reset_index(drop=True)
+        dataset = self.fill_values_with_none(dataset)
+        dataset = self.fill_values_with_zero(dataset)
+        dataset = self.fill_values_with_mode(dataset)
+        dataset = self.fill_values_with_median(dataset)
+        dataset = self.column_values_to_str(dataset)
+        dataset = self.drop_values(dataset)
+        dataset = self.create_new_variable(dataset)
 
-class TrainDataset(Dataset):
+        numeric_feats = dataset.dtypes[dataset.dtypes != "object"].index
+        skewed_feats = dataset[numeric_feats].apply(
+            lambda x: skew(x.dropna())).sort_values(ascending=False)
+        skewness = pd.DataFrame({'Skew': skewed_feats})
+        skewness = skewness[abs(skewness) > 0.75]
+        skewed_features = skewness.index
+        lam = 0.15
+        for feat in skewed_features:
+            dataset[feat] = boxcox1p(dataset[feat], lam)
 
-    def remove_garage_area_outliers(self):
-        self.dataset = self.dataset[self.dataset['GarageArea'] < 1200]
+        self.create_categorical_columns(dataset)
+        self.create_numeric_columns(dataset)
 
-    def remove_null_values(self):
-        self.dataset = self.dataset.select_dtypes(
-            include=[np.number]).interpolate().dropna()
+        self.train = dataset[:ntrain]
+        self.test = dataset[ntrain:]
+        self.targets = pd.DataFrame(np.log(y_train))
 
-    def create_targets(self):
-        self.targets = np.log(self.dataset.SalePrice)
-
-    def drop_columns(self):
-        self.dataset = self.dataset.drop(['SalePrice', 'Id'], axis=1)
-
-    def create_dataset(self):
-        super().create_dataset()
-
-        self.remove_garage_area_outliers()
-        self.remove_null_values()
-        self.create_targets()
-        self.drop_columns()
-
-        if self.verbose:
-            self.print_shape()
-
-        return self.dataset
-
-
-class TestDataset(Dataset):
-
-    def select_features(self):
-        ids = self.dataset['Id']
-
-        self.dataset = self.dataset.select_dtypes(
-            include=[np.number]).drop(['Id'], axis=1).interpolate()
-
-        self.dataset['Id'] = ids
-
-    def create_dataset(self):
-        super().create_dataset()
-
-        self.select_features()
-
-        if self.verbose:
-            self.print_shape()
-
-        return self.dataset
+        return self.train, self.test, self.targets, self.test_id
