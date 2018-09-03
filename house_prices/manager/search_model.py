8602a815a8d9a76ba045590f6f9b165e48ab96e5
Lucas Moura <lucas.moura128@gmail.com>
2018-09-03 17:11:25 -0300

    Update search_model module

--- house_prices/manager/search_model.py
+++ house_prices/manager/search_model.py
@@ -3,81 +3,73 @@ import inspect
 import pkgutil
 
 
-INVALID_MODEL_NAME = -1
-INVALID_PIPELINE_NAME = -2
+INVALID_PATH = -1
 
 
-def iterate_over_module(path):
-    if type(path) != list:
-        path = [path]
+class ClassSearcher:
 
-    return pkgutil.iter_modules(path)
+    def __init__(self, folder_to_look):
+        self.folder_to_look = folder_to_look
+        self.path = None
 
+    def iterate_over_module(self, path):
+        if type(path) != list:
+            path = [path]
 
-def create_import_name(model_path, name):
-    return model_path.replace('/', '.') + '.' + name
+        return pkgutil.iter_modules(path)
 
+    def create_import_name(self, name, folder):
+        return folder.replace('/', '.') + '.' + name
 
-def find_model(model_name, module='models'):
-    for finder, name, ispkg in iterate_over_module(module):
-        if name == model_name and ispkg:
-            return create_import_name(module, name)
+    def find_module(self, module_name):
+        parsed_look_path = self.folder_to_look.replace('.', '/')
 
-    return None
+        for finder, name, ispkg in self.iterate_over_module(parsed_look_path):
+            if name == module_name and ispkg:
+                return self.create_import_name(name, self.folder_to_look)
 
+        return None
 
-def find_pipeline(pipeline_name, model_path):
-    pipeline_path = create_import_name(model_path, 'pipelines')
-    pipeline_file_path = pipeline_path.replace('.', '/')
+    def get_class(self, name):
+        self.path = self.find_module(name)
 
-    for finder, name, ispkg in iterate_over_module(pipeline_file_path):
-        if name == pipeline_name and ispkg:
-            return create_import_name(pipeline_path, name)
+        if not self.path:
+            return INVALID_PATH
 
-    return None
+        return self.load_module(self.path)
 
+    def load_module_classes(self, module_path):
+        module = importlib.import_module(module_path)
+        return inspect.getmembers(module, inspect.isclass)
 
-def load_module_classes(path):
-    module = importlib.import_module(path)
-    return inspect.getmembers(module, inspect.isclass)
+    def module_defined_classes(self, classes, module_path):
+        return [class_obj for _, class_obj in classes
+                if class_obj.__module__ == module_path]
 
-
-def load_model(model_path, module_name='model'):
-    model_path = create_import_name(model_path, module_name)
-    model_class = load_module_classes(model_path)[0][1]
-
-    return model_class
-
-
-def module_defined_classes(classes, module_name):
-    return [class_obj for _, class_obj in classes
-            if class_obj.__module__ == module_name]
-
-
-def sort_operations(operations_classes):
-    return sorted(operations_classes, key=lambda x: x.ORDER)
+    def load_module(self, module_path):
+        module_path = self.create_import_name(self.default_module, module_path)
+        model_classes = self.load_module_classes(module_path)
+        return self.module_defined_classes(model_classes, module_path)
 
 
-def load_pipeline_operations(pipeline_path, module_name='pipeline'):
-    pipeline_path = create_import_name(pipeline_path, module_name)
+class ModelSearcher(ClassSearcher):
+    def __init__(self, folder_to_look):
+        super().__init__(folder_to_look)
+        self.default_module = 'model'
 
-    operations_classes = load_module_classes(pipeline_path)
-    operations_defined_classes = module_defined_classes(operations_classes, pipeline_path)
+    def load_module(self, module_path):
+        module_defined_classes = super().load_module(module_path)
+        return module_defined_classes[0]
 
-    return sort_operations(operations_defined_classes)
 
+class PipelineSearcher(ClassSearcher):
+    def __init__(self, folder_to_look):
+        super().__init__(folder_to_look)
+        self.default_module = 'pipeline'
 
-def get_model_objects(model_name, pipeline_name):
-    model_path = find_model(model_name)
-    if not model_path:
-        return INVALID_MODEL_NAME
+    def sort_operations(self, operations_classes):
+        return sorted(operations_classes, key=lambda x: x.ORDER)
 
-    model = load_model(model_path)
-
-    pipeline_path = find_pipeline(pipeline_name, model_path)
-    if not pipeline_path:
-        return INVALID_PIPELINE_NAME
-
-    pipeline_operations = load_pipeline_operations(pipeline_path)
-
-    return (model, pipeline_operations)
+    def load_module(self, module_path):
+        module_defined_classes = super().load_module(module_path)
+        return self.sort_operations(module_defined_classes)
--- house_prices/tests/test_search_model.py
+++ house_prices/tests/test_search_model.py
@@ -1,6 +1,6 @@
 import unittest
 
-from manager.search_model import find_model, find_pipeline, load_pipeline_operations, load_model
+from manager.search_model import ModelSearcher, PipelineSearcher
 
 from tests.test_models.linear_regression.model import Test
 from tests.test_models.linear_regression.pipelines.p1.pipeline import (LFillMissing,
@@ -13,44 +13,48 @@ from tests.test_models.linear_regression.pipelines.p1.pipeline import (LFillMiss
 class TestSearchModel(unittest.TestCase):
 
     def test_find_model(self):
+        model_searcher = ModelSearcher('tests.test_models')
         model_name = 'linear_regression'
-        model_path = find_model(model_name, module='tests/test_models')
+        model_path = model_searcher.find_module(model_name)
         self.assertEqual(model_path, 'tests.test_models.linear_regression')
 
         model_name = 'random_forest'
-        model_path = find_model(model_name, module='tests/test_models')
+        model_path = model_searcher.find_module(model_name)
         self.assertEqual(model_path, 'tests.test_models.random_forest')
 
         model_name = 'neural_network'
-        model_path = find_model(model_name, module='tests/test_models')
+        model_path = model_searcher.find_module(model_name)
         self.assertEqual(model_path, None)
 
     def test_find_pipeline(self):
-        model_path = 'tests.test_models.linear_regression'
+        folder_to_look = 'tests.test_models.linear_regression.pipelines'
+        pipeline_searcher = PipelineSearcher(folder_to_look)
         pipeline_name = 'p1'
-        pipeline_path = find_pipeline(pipeline_name, model_path)
+        pipeline_path = pipeline_searcher.find_module(
+            pipeline_name)
         self.assertEqual(
             pipeline_path,
             'tests.test_models.linear_regression.pipelines.p1'
         )
 
-        model_path = 'tests.test_models.linear_regression'
         pipeline_name = 'p5'
-        pipeline_path = find_pipeline(pipeline_name, model_path)
+        pipeline_path = pipeline_searcher.find_module(
+            pipeline_name)
         self.assertEqual(
             pipeline_path,
             None
         )
 
     def test_load_pipeline_operations(self):
-        pipeline_path = 'tests.test_models.linear_regression.pipelines.p1'
+        folder_to_look = 'tests.test_models.linear_regression.pipelines'
+        pipeline_searcher = PipelineSearcher(folder_to_look)
         expected_order = [LFillMissing, LTransformations, LCreate, LDrop, LFinalize]
 
-        operations = load_pipeline_operations(pipeline_path)
+        operations = pipeline_searcher.get_class('p1')
         self.assertEqual(operations, expected_order)
 
     def test_load_model(self):
-        model_path = 'tests.test_models.linear_regression'
-        model = load_model(model_path)
+        model_searcher = ModelSearcher('tests.test_models')
+        model = model_searcher.get_class('linear_regression')
 
         self.assertEqual(model, Test)
