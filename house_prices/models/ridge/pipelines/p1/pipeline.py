c132fa8fe61f966096c4efa3c43e174238534ab8
Lucas Moura <lucas.moura128@gmail.com>
2018-09-25 11:38:18 -0300

    Update pipeline for ridge model

--- house_prices/models/ridge/pipelines/p1/pipeline.py
+++ house_prices/models/ridge/pipelines/p1/pipeline.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from models.base_pipeline import (BaseFillMissing, BaseTransformations,
                                   BaseCreate, BaseDrop, BaseTargetTransform,
                                   BaseFinalize, BasePredictionTransform)
@@ -17,11 +19,23 @@ class RCreate(BaseCreate):
         self.category_columns.remove('GarageYrBlt')
 
 
+    def create_age_at_selling_point(self):
+        for dataset in self.loop_datasets():
+            dataset['age_selling_point'] = dataset['YrSold'] - dataset['YearBuilt']
+
+
+    def create_time_since_remodelled(self):
+        for dataset in self.loop_datasets():
+            dataset['time_since_remodelled'] = dataset['YrSold'] - dataset['YearRemodAdd']
+
+
 class RDrop(BaseDrop):
     def __init__(self):
         super().__init__()
 
-        self.drop_columns.append('GarageYrBlt')
+        self.drop_columns.extend(
+            ['YearBuilt', 'YearRemodAdd', 'YrSold', 'GarageYrBlt']
+        )
 
 
 class RTargetTransform(BaseTargetTransform):
